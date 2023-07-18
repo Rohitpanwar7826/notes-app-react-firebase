9f9e8256a701a5c557b4392925afb52f39f0e274
Rohit Singh Panwar <rohit.panwar@gemsessence.com>
2023-07-19 01:26:40 +0530

    Added loading on sign-page

--- src/components/SignInOptions/Index.jsx
+++ src/components/SignInOptions/Index.jsx
@@ -1,10 +1,15 @@
 import { signInWithPopup } from "firebase/auth";
 import { auth, googleProvider } from "../../firebase";
 import { toast } from "react-toastify";
-import './index.css';
+import "./index.css";
+import { useState } from "react";
+import Loader from '../shared/Loader';
 
 const SignInOptions = () => {
+  const [loading, setLoading] = useState(false);
+
   const signInWithGoogle = async () => {
+    setLoading(true);
     await signInWithPopup(auth, googleProvider)
       .then((user) => {
         toast.success(`Hi, ${user?.user?.displayName}`, {
@@ -17,8 +22,10 @@ const SignInOptions = () => {
           progress: undefined,
           theme: "light",
         });
+        setLoading(false);
       })
       .catch((error) => {
+        setLoading(false);
         console.error(error);
       });
   };
@@ -27,9 +34,13 @@ const SignInOptions = () => {
     <div className="google-login d-flex align-items-center justify-content-center bg-light">
       <div className="text-center">
         <h1>Google Login</h1>
-        <button className="btn btn-primary" onClick={signInWithGoogle}>
-          Sign In with Google
-        </button>
+        {loading ? (
+          <Loader />
+        ) : (
+          <button className="btn btn-primary" onClick={signInWithGoogle}>
+            Sign In with Google
+          </button>
+        )}
       </div>
     </div>
   );
